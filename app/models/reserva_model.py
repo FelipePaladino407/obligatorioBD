762c766094911bd681fc6348cad937d4e149ac55
Felipe <felipaladino05@gmail.com>
2025-10-18 18:40:13 -0300

    reserv

---
+++ app/models/reserva_model.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclasses
+from datetime import date 
+from typing import TypeDict, List
+
+@datacass(frozen=True)
+
+# Modelo de entrada: Representa lo que se necesita para crear una reserva nueva en el sistema.
+class ReservaCreate:
+    nombre_sala: str
+    edificio: str
+    fecha: date 
+    id_turno: int
+    participantes_ci: List[str]
+
+# Modelo de infame salida: Representa una fila real de la tabla "reserva" en la base del dato.
+class ReservaRow(TypeDict): 
+    id_reserva: int
+    nombre_sala: str
+    edificio: str
+    fecha: date 
+    id_turno: int
+    estado: str # 'activa' | 'cancelada' | 'sin_asistencia' | 'finalizada'
+
